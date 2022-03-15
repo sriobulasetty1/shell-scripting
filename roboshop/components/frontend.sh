06ee694a2ab8f0365e6e16e55815aafa7555c392
Sri <sri_obulasetty@hotmail.com>
2022-03-15 05:45:45 -0700

    yes2

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,20 +1,7 @@
 #!/bin/bash
 
-StatCheck() {
-  if [ "$1" -eq 0 ];then
-    echo SUCCESS
-    else
-      echo FAILURE
-      exit 2
-      fi
-      }
-
-print() {
-  echo -e "====================$1==============" &>>$LOG_FILE
-  echo -e "\e[36m $1 \e[0m"
-}
-
-LOG_FILE=/tmp/roboshop.log
+#we have to use source command because it has functions and other variables.
+source components/common.sh
 print "installing nginx"
 yum install nginx -y &>> $LOG_FILE
 StatCheck $?
