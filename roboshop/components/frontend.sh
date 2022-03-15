0f98bf916c77aac883d71f1fc1f993ed201c10ce
Sri <sri_obulasetty@hotmail.com>
2022-03-15 05:01:05 -0700

    yes2

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -7,12 +7,30 @@ if [ $? -ne 0 ]; then
     echo FAILURE
    exit 1
 fi
+
+#Function
+Print_message() {
+  echo Hello
+  }
+Print_message
+
+StatCheck() {
+  if [ $1 -eq 0];then
+    echo SUCCESS
+    else
+      echo FAILURE
+      exit 2
+      fi
+      }
+
+}
 echo -e "\e[36m Installing nginx \e[0m"
 yum install nginx -y
-
+StatCheck $?
 
 echo -e "\e[36m Download nginx \e[0m"
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
+StatCheck $?
 
 echo -e "\e[36m Update nginx \e[0m"
 rm -rf /usr/share/nginx/html/*
@@ -22,7 +40,9 @@ mv frontend-main/* .
 mv static/* .
 rm -rf frontend-main README.md
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
+StatCheck $?
 
 echo -e "\e[36m Restart nginx \e[0m"
 systemctl restart nginx
 systemctl enable nginx
+StatCheck $?
