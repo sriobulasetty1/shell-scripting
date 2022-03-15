fcdbe884a2c6576787a788ae218dd4c27574b949
Sri <sri_obulasetty@hotmail.com>
2022-03-15 05:22:45 -0700

    yes2

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -9,20 +9,23 @@ StatCheck() {
       fi
       }
 
+Print() {
+  echo -e "\e[36m $1 \e[0m"
+}
 
-echo -e "\e[36m Installing nginx \e[0m"
+print installing nginx
 yum install nginx -y
 StatCheck $?
 
-echo -e "\e[36m Download nginx \e[0m"
+print download
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
 StatCheck $?
 
-echo -e "\e[36m Update nginx \e[0m"
+print cleanup
 rm -rf /usr/share/nginx/html/* && cd /usr/share/nginx/html && unzip /tmp/frontend.zip && mv frontend-main/* . && mv static/* . && rm -rf frontend-main README.md && mv localhost.conf /etc/nginx/default.d/roboshop.conf
 StatCheck $?
 
-echo -e "\e[36m Restart nginx \e[0m"
+print start
 systemctl restart nginx
 systemctl enable nginx
 StatCheck $?
