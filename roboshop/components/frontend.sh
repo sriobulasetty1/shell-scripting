a003d12d726041bddc2489fd3572d767955749a0
Sri <sri_obulasetty@hotmail.com>
2022-03-15 05:23:44 -0700

    yes2

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -13,19 +13,19 @@ Print() {
   echo -e "\e[36m $1 \e[0m"
 }
 
-print installing nginx
+print "installing nginx"
 yum install nginx -y
 StatCheck $?
 
-print download
+print "download"
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
 StatCheck $?
 
-print cleanup
+print "cleanup"
 rm -rf /usr/share/nginx/html/* && cd /usr/share/nginx/html && unzip /tmp/frontend.zip && mv frontend-main/* . && mv static/* . && rm -rf frontend-main README.md && mv localhost.conf /etc/nginx/default.d/roboshop.conf
 StatCheck $?
 
-print start
+print "start"
 systemctl restart nginx
 systemctl enable nginx
 StatCheck $?
