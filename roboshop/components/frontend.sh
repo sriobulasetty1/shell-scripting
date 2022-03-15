42f02bb312c3c70e247fd835a8bbacd3b5d3b8c7
Sri <sri_obulasetty@hotmail.com>
2022-03-15 04:19:36 -0700

    yes2

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
+echo "Installing nginx"
 yum install nginx -y
 
 
-print Download Nginx
+echo "Download"
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
 
-
+echo "Cleanup"
 rm -rf /usr/share/nginx/html/*
 cd /usr/share/nginx/html/
 unzip /tmp/frontend.zip
@@ -15,5 +16,6 @@ mv static/* .
 rm -rf frontend-main README.md
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
 
+echo "restart"
 systemctl restart nginx
 systemctl enable nginx
