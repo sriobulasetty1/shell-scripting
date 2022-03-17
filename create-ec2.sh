9ed1e8d5eba3a5da02811d6fec7680c54cd9a256
Sri <sri_obulasetty@hotmail.com>
2022-03-17 15:34:54 -0700

    yes5

--- create-ec2.sh
+++ create-ec2.sh
@@ -10,7 +10,7 @@ COMPONENT=$1
 ZONE_ID="Z10446201CHRBX2OFKX"
 
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" | jq '.Images[].ImageId' | sed -e 's/"//g')
-SGID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=allow-all-from-public | jq '.SecurityGroups[].GroupdId'| sed -e 's/"//g')
+SGID=$(aws ec2 describe-security-groups --filters "Name=group-name,Values=allow-all-from-public" | jq '.SecurityGroups[].GroupdId'| sed -e 's/"//g')
 
 echo $AMI_ID
 PRIVATE_IP=${aws ec2 run-instances \
