a88a2dba705553fc6be42d7fe007138b7c492849
Sri <sri_obulasetty@hotmail.com>
2022-03-17 13:27:55 -0700

    yes2

---
+++ create-ec2.sh
@@ -0,0 +1,21 @@
+if [ -z "$1" ]; then
+  echo -e "Input Machine Name"
+  exit 1
+fi
+COMPONENT=$1
+ZONE_ID="Z10446201CHRBX2OFKX"
+
+AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=CentOS-7-DevOps-Practise"|jq '.Images[].ImageId'|sed -e 's/"//g')
+SGID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=allow-all-from-public|jq '.SecurityGroups[].GroupdId'|sed -e 's/"//g')
+
+echo $AMI_ID
+PRIVATE_IP ${aws ec2 run-instances \
+    --image-id ${AMI_ID} \
+    --Instance-type t2.micro \
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Values=${COMPONENT}}]" \
+    --instance-market-options "MarketTypes=SpotOptions={$SpotInstanceType=presistent,InstanceInterruptionBehavior=stop}"\
+    --security-group-ids ${SGID} \
+    |jq '.Instances[].PrivateIpAddress'|sed-e 's/"//g'}
+
+  sed -e "s/IPADDRESS/${PRIVATE_IP}/" -e "s/COMPONENT/${COMPONENT}/" route53.json > /tmp/record.json
+  aws route53 change-resource-record-sets --hosted-zone-id ${ZONE_ID} --change-batch file:///tmp/record.json | jq
--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -13,5 +13,14 @@ print() {
   echo -e "====================$1==============" &>>$LOG_FILE
   echo -e "\e[36m $1 \e[0m"
 }
-
 LOG_FILE=/tmp/roboshop.log
+
+print "Setup SystemD replace old with new hostnames"
+sed -i -e 's/abc/xyz/' \
+       -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal' \
+       home/roboshop/${COMPONENT}/systemd.service
+print "System service start and daemon reload"
+
+SERVICE_SETUP() {
+ systemctl daemon-reload &>>$LOGILFE && systemctl restart ${COMPONENT}.service
+ }
