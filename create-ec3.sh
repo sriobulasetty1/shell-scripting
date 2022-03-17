5f99a2aedf5f7a512b08a1918d7c74abd42cb40c
Sri <sri_obulasetty@hotmail.com>
2022-03-17 16:06:12 -0700

    yes5

--- create-ec3.sh
+++ create-ec3.sh
@@ -11,5 +11,4 @@ AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Pra
 
 echo $AMI_ID
 aws ec2 run-instances --image-id ${AMI_ID} --instance-type t2.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}]"
-aws ec2 describe-security-groups --filters "Name=group-name,Values=allow-all-from-public" | jq '.SecurityGroups[].GroupId' | sed -e 's/"//g')
 
