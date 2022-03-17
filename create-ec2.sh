0f4c1973dd944bdac6ad34fddc56cf2e389602c1
Sri <sri_obulasetty@hotmail.com>
2022-03-17 15:44:29 -0700

    yes5

--- create-ec2.sh
+++ create-ec2.sh
@@ -13,7 +13,7 @@ AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Pra
 SGID=$(aws ec2 describe-security-groups --filters "Name=group-name,Values=allow-all-from-public" | jq '.SecurityGroups[].GroupId' | sed -e 's/"//g')
 
 ceate_ec2() {
-PRIVATE_IP=${aws ec2 run-instances \
+PRIVATE_IP=$(aws ec2 run-instances \
     --image-id ${AMI_ID} \
     --Instance-type t2.micro \
     --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" \
