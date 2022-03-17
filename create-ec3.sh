d8798b4c4551546c64833d3f99fc29c434345b76
Sri <sri_obulasetty@hotmail.com>
2022-03-17 16:11:42 -0700

    yes5

--- create-ec3.sh
+++ create-ec3.sh
@@ -10,5 +10,5 @@ COMPONENT=$1
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" | jq '.Images[].ImageId' | sed -e 's/"//g')
 
 echo $AMI_ID
-aws ec2 run-instances --image-id ${AMI_ID} --instance-type t2.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]
+aws ec2 run-instances --image-id ${AMI_ID} --instance-type t2.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]"
 
