ee0d4218e3b06792f21f07444a87e397cb6f79c8
Sri <sri_obulasetty@hotmail.com>
2022-03-18 14:34:52 -0700

    yes5

---
+++ frontend.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#we have to use source command because it has functions and other variables.
+source components/common.sh
+print "installing nginx"
+yum install nginx -y &>> $LOG_FILE
+StatCheck $?
+
+print "download"
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>> $LOG_FILE
+StatCheck $?
+
+print "cleanup"
+rm -rf /usr/share/nginx/html/* && cd /usr/share/nginx/html && unzip /tmp/frontend.zip &>> $LOG_FILE && mv frontend-main/* . && mv static/* . && rm -rf frontend-main README.md && mv localhost.conf /etc/nginx/default.d/roboshop.conf &>> $LOG_FILE
+StatCheck $?
+
+print "start"
+systemctl restart nginx
+systemctl enable nginx
+StatCheck $?
